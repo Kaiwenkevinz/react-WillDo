cd016d556f1099825aabd478f178357a031ba07f
Kaiwenkevinz <kevin.zhangkw@gmail.com>
2018-11-28 16:48:22 -0700

    some logic done

--- will-do-app/src/Main/index.js
+++ will-do-app/src/Main/index.js
@@ -2,6 +2,58 @@ import React, { Component } from 'react';
 import InputMenu from '../InputMenu/index';
 
 class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        tag: '',
+        key: '',
+      }
+    }
+  }
+
+  // input field onChange listener
+  handleOnChange = (e) => {
+    const currentItem = {
+      text: e.target.value,
+      tag: 'z',
+      key: Date.now()
+    }
+    this.setState({
+      currentItem
+    })
+  }
+
+  // Catagory Dropup Menu listener
+  handleDropUpMenu = (e) => {
+    console.log()
+  }
+
+
+  // Add Task button listener
+  handleAddItem = (e) => {
+    e.preventDefault()
+
+    // update items
+    const items = this.state.items
+    items.push(this.state.currentItem)
+    this.setState({
+      items
+    })
+
+    // empty current item
+    let currentItem = this.state.currentItem
+    currentItem = {
+      text: '',
+    }
+    this.setState({
+      currentItem
+    })
+
+    console.log(this.state.items)
+  }
 
   render() {
     return(
@@ -12,7 +64,11 @@ class Main extends Component {
         <div id="sectionPurple"></div>
         <div id="sectionBlue"></div>
 
-          <InputMenu />
+          <InputMenu
+            handleOnChange={this.handleOnChange}
+            handleAddItem={this.handleAddItem}
+            currentItem={this.state.currentItem}
+          />
 
       </div>
     )
