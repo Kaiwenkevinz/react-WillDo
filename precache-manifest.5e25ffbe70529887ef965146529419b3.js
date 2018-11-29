efadb9a09d61ee277b6154dd238197875e39c9b9
Kaiwenkevinz <kevin.zhangkw@gmail.com>
2018-11-28 21:00:47 -0700

    Updates

---
+++ precache-manifest.5e25ffbe70529887ef965146529419b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bb3dabe8fb5881a8628",
+    "url": "/react-WillDo/static/js/runtime~main.1bb3dabe.js"
+  },
+  {
+    "revision": "253b6351c804ec3e639c",
+    "url": "/react-WillDo/static/js/main.253b6351.chunk.js"
+  },
+  {
+    "revision": "6ea4476df7c52da542d9",
+    "url": "/react-WillDo/static/js/1.6ea4476d.chunk.js"
+  },
+  {
+    "revision": "253b6351c804ec3e639c",
+    "url": "/react-WillDo/static/css/main.39256f39.chunk.css"
+  },
+  {
+    "revision": "68cf5157c173f645a7cd2f17d3d093c1",
+    "url": "/react-WillDo/index.html"
+  }
+];
